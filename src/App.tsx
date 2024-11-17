3f42e19425f6632e0e130dcbdf7fff1608f3c325
Sahil Pingale <sahilpingale@gmail.com>
2024-11-17 17:13:35 +0530

    Mantine setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
-
+import { Button } from "@mantine/core"
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<>123</>} />
+      <Route
+        path="/"
+        element={
+          <>
+            <Button>Button</Button>
+          </>
+        }
+      />
       <Route path="/test" element={<>test</>} />
     </Routes>
   )
--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,16 @@ import "./index.css"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
 import { BrowserRouter } from "react-router-dom"
+import { MantineProvider } from "@mantine/core"
+import "@mantine/core/styles.css"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <MantineProvider>
+        <App />
+      </MantineProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
